e4b655dadaee4e5f438867da96d66c7d345d2810
43695512 <yanter@wp.pl>
2022-03-06 19:08:04 +0100

    Sorting data and cleaning code.

--- README.md
+++ README.md
@@ -11,9 +11,10 @@ The goal is to write an application that will communicate with this API and have
 3. Clicking the button again should replace the previous user with the new one. ✔️
 4. Add checkbox "Hide address". If the field is checked, you shouldn't fetch or display an address for this user. ✔️
 5. Only fetch the data you need. ✔️
-6. Create a separate page to display a table with the last 10 generated users. Display first name, last name, country, and registration date. ✔️/❌
-7. Allow sorting table by last name and registration date. ❌
+6. Create a separate page to display a table with the last 10 generated users. Display first name, last name, country, and registration date. ✔️
+7. Allow sorting table by last name and registration date. ✔️
 8. Data in the table should be saved after leaving the page (you can use e.g. localStorage) and set on init. ✔️
+9. Make it look good. ❌
 
 # Requirements
 
--- script.js
+++ script.js
@@ -66,16 +66,26 @@ function show(data) {
   // Setting innerHTML as tab variable
   document.querySelector("#test").innerHTML = tab;
 }
+// 0 - no sorting, 1 - sort by last name, 2 - sort by register data, 3 - sort by both
+var sortType = 0;
 
-function hideLocationInfo() {
+function changeState(clickedId) {
   // Get the checkbox
-  var checkBox = document.getElementById("checkbox");
+  console.log(clickedId);
+  var checkBox = document.getElementById(clickedId);
   // If the checkbox is checked, display the output text
   if (checkBox.checked == true) {
     showAdresses = false;
+    if (clickedId == "lastncb") sortType++;
+    if (clickedId == "registercb") sortType += 2;
+    console.log("sort: ", sortType);
   } else {
     showAdresses = true;
+    if (clickedId == "lastncb") sortType--;
+    if (clickedId == "registercb") sortType -= 2;
+    console.log("sort: ", sortType);
   }
+  displayPeopleList();
 }
 
 function getPeopleList() {
@@ -101,24 +111,41 @@ function updatePeopleList(data) {
   localStorage.setItem("peopleList", JSON.stringify(people));
 }
 
+function sortPeople() {}
+console.log("sorting~!@#");
+//Display first name, last name, country, and registration date.
 function displayPeopleList() {
-  //Display first name, last name, country, and registration date.
   var people = getPeopleList();
   let tab = "";
+  //Allow sorting table by last name and registration date.
+  if (sortType != 0) {
+    people.sort(function (a, b) {
+      let x = a.name.last.toUpperCase(),
+        y = b.name.last.toUpperCase(),
+        z = formatDate(a.registered.date);
+      c = formatDate(b.registered.date);
+      console.log(z, c);
+      //Sort by last name
+      if (sortType != 2) {
+        return x == y ? (sortType == 3 ? (z > y ? 1 : -1) : 0) : x > y ? 1 : -1;
+      } else {
+        return z == c ? 0 : z > c ? 1 : -1;
+      }
+    });
+  }
+
   people.forEach(function (user, index) {
     var registerDate = formatDate(user.registered.date);
 
     tab += `
-    <div>
+    <div class="listItem">
     <h3>${index + 1}.</h3>
     <img src="${user.picture.large}"/>
-    <ul>
-      <li>First Name: ${user.name.first}</li>
-      <li>Last Name: ${user.name.last}</li>
-      <li>Register Date: ${registerDate}</li>
-      <li>Nationality: ${user.nat}</li>
-    </ul>
-    <div>`;
+      <p>First Name: ${user.name.first}</p>
+      <p>Last Name: ${user.name.last}</p>
+      <p>Register Date: ${registerDate}</p>
+      <p>Nationality: ${user.nat}</p>
+    </div>`;
   });
   document.querySelector("#other").innerHTML = tab;
 }
